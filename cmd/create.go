ab3d52af1cf182ddccd89c3f268b04d09734f6c8
Steve Garf <stgarf@gmail.com>
2019-03-03 09:01:40 -0800

    Fix 'replace' flag bug and remove long description

--- cmd/create.go
+++ cmd/create.go
@@ -25,17 +25,13 @@ import (
 	"github.com/spf13/viper"
 )
 
+var replace bool
+
 // createCmd represents the create command
 var createCmd = &cobra.Command{
 	Use:     "create",
 	Aliases: []string{"c", "cr"},
-	Short:   "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short:   "Create a new configuration file",
 	Run: func(cmd *cobra.Command, args []string) {
 		//replace, _ := cmd.Flags().GetBool("replace")
 		log.Debugf("Called 'create' with args %v, replace: %v", args, replace)
@@ -80,5 +76,5 @@ to quickly create a Cobra application.`,
 
 func init() {
 	configCmd.AddCommand(createCmd)
-	createCmd.Flags().BoolP("replace", "r", false, "Replace/delete an existing .paperless-cli.yaml")
+	createCmd.Flags().BoolVarP(&replace, "replace", "r", false, "Replace/delete an existing .paperless-cli.yaml")
 }
