d30d2a354f502082dbd3cd719ad492d0358843a3
Steve Garf <stgarf@gmail.com>
2019-03-05 09:08:13 -0800

    Create a 'search' subcommand to search tags in a Paperless instance.
    * Uses convenience functions from paperless package.
    * Search can be either case sensitive or not by passing a flag.

---
+++ cmd/tagSearch.go
@@ -0,0 +1,57 @@
+// Copyright © 2019 Steve Garf <stgarf@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var name string
+var caseSensitive bool
+
+// tagSearchCmd represents the tagSearch command
+var tagSearchCmd = &cobra.Command{
+	Use:     "search",
+	Aliases: []string{"s"},
+	Short:   "Search for tag by name",
+	Args:    cobra.NoArgs,
+	// 	Long: `A longer description that spans multiple lines and likely contains examples
+	// and usage of using your command. For example:
+
+	// Cobra is a CLI library for Go that empowers applications.
+	// This application is a tool to generate the needed files
+	// to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		//fmt.Printf("tagSearch called, term: %v, case sensitive?: %v\n", name, caseSensitive)
+		tags, err := PaperInst.GetTag(name, caseSensitive)
+		if err != nil {
+			log.Fatalf("Error %v", err)
+		}
+		fmt.Printf("%v results found:\n", len(tags))
+		for _, tag := range tags {
+			fmt.Println(tag)
+		}
+	},
+}
+
+func init() {
+	tagSearchCmd.Flags().StringVarP(&name, "name", "n", "", "Name of the tag to search for (required)")
+	tagSearchCmd.MarkFlagRequired("name")
+	tagSearchCmd.Flags().BoolVarP(&caseSensitive, "case_sensitive", "i", false, "Enable case sensitivity")
+	tagsCmd.AddCommand(tagSearchCmd)
+}
