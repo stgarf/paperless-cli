7d7f11c7e8e793d45e60f62d9b8619450a4a6aac
Steve Garf <stgarf@gmail.com>
2019-03-10 09:35:24 -0700

    Update README docs, usage info, example config, and build script (#4)

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -euf -o pipefail
 
-GOVVVBIN=$(which govvv) || GOVVVBIN=${GOVVVBIN:=$GOPATH/bin/govvv}
-GOBIN=$(which go) || GOBIN=${GOBIN:=$GOROOT/bin/go}
-ECHOBIN=$(which echo)
-SEDBIN=$(which sed)
+GOBIN=$(command -v go) || GOBIN=${GOBIN:=$GOROOT/bin/go}
+ECHOBIN=$(command -v echo)
+command -v govvv >/dev/null 2>&1 || {
+    $ECHOBIN -en 'Need govvv, building and installing..';
+    $GOBIN install github.com/ahmetb/govvv;
+    $ECHOBIN '. Done.';
+} 
+GOVVVBIN=$(command -v govvv) || GOVVVBIN=${GOVVVBIN:=$GOPATH/bin/govvv}
+SEDBIN=$(command -v sed)
 
 GOPKGPATH=$($GOBIN list ./cmd)
 GOLDFLAGS=$($GOVVVBIN -flags)
 GOLDFLAGS=$($ECHOBIN $GOLDFLAGS | $SEDBIN -e 's|main|'$GOPKGPATH'|g')
 
-$ECHOBIN "Building..."
+$ECHOBIN -ne "Building.."
 $GOBIN build -ldflags="$GOLDFLAGS" -o bin/paperless-cli
-$ECHOBIN "Build complete."
+$ECHOBIN ". Build complete."
